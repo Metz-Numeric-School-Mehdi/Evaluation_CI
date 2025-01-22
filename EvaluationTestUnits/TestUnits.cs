06c9fd4cf075d820a3712048a3286fe18c813d88
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-01-22 16:31:10 +0100

    ✨ feat: add custom stack

--- EvaluationTestUnits/TestUnits.cs
+++ EvaluationTestUnits/TestUnits.cs
@@ -7,16 +7,18 @@ public sealed class TestUnits
 {
     private MathOperations _mathOperations;
     private HtmlFormatHelper _htmlFormatHelper;
+    private CustomStack _stack;
 
     [TestInitialize]
     public void Initialize()
     {
         _mathOperations = new MathOperations();
         _htmlFormatHelper = new HtmlFormatHelper();
+        _stack = new CustomStack();
     }
-    
+
     // Tests Math Operation
-    
+
     // Add Method
     [TestMethod]
     [DataRow(14, 22, 36)]
@@ -27,7 +29,7 @@ public sealed class TestUnits
         var result = _mathOperations.Add(numberOne, numberTwo);
         Assert.AreEqual(expectedResult, result);
     }
-    
+
     // Divide Method
     [TestMethod]
     [DataRow(10, 10, 1)]
@@ -38,7 +40,7 @@ public sealed class TestUnits
         var result = _mathOperations.Divide(numberOne, numberTwo);
         Assert.AreEqual(expectedResult, result);
     }
-    
+
     //Get Odd Number Method
     // [TestMethod]
     // [DataRow(10, 10, 1)]
@@ -49,9 +51,9 @@ public sealed class TestUnits
     //     var result = _mathOperations.Divide(numberOne, numberTwo);
     //     Assert.AreEqual(expectedResult, result);
     // }
-    
+
     // Tests HTML Formater
-    
+
     // Get Bold Format Method 
     [TestMethod]
     public void GetBoldFormat_WithHTMLContent_ReturnsBoldFormat()
@@ -59,7 +61,7 @@ public sealed class TestUnits
         var result = _htmlFormatHelper.GetBoldFormat("Content");
         Assert.AreEqual("<b>Content</b>", result);
     }
-    
+
     // Get Italic Format Method 
     [TestMethod]
     public void GetItalicFormat_WithHTMLContent_ReturnsItalicFormat()
@@ -67,7 +69,7 @@ public sealed class TestUnits
         var result = _htmlFormatHelper.GetItalicFormat("Content");
         Assert.AreEqual("<i>Content</i>", result);
     }
-    
+
     // Get Formatted List Elements Method 
     [TestMethod]
     public void GetFormattedListElements_WithHTMLContents_ReturnsFormattedListElements()
@@ -75,7 +77,85 @@ public sealed class TestUnits
         var contents = new List<string> { "Content", "Test" };
         var result = _htmlFormatHelper.GetFormattedListElements(contents);
         var expected = "<ul><li>Content</li><li>Test</li></ul>";
-        
+
         Assert.AreEqual(expected, result);
+    }
+
+    // Tests Custom Stack
+
+    // Pop Method
+    [TestMethod]
+    public void Pop_WithEmptyValue_ReturnsException()
+    {
+        Assert.ThrowsException<CustomStack.StackCantBeEmptyException>(() => _stack.Pop());
+    }
+
+    [TestMethod]
+    public void Pop_WithListOfValue_ReturnsListOfPoppedValue()
+    {
+        _stack.Push(10);
+        _stack.Push(11);
+        _stack.Push(12);
+
+        var result = _stack.Pop();
+        Assert.AreEqual(12, result);
+    }
+
+    // Push Method
+    [TestMethod]
+    public void Push_WithValidValue_IncreasesCountAndAddsElementToStack()
+    {
+        Assert.AreEqual(0, _stack.Count());
+        _stack.Push(10);
+
+        Assert.AreEqual(1, _stack.Count());
+        var poppedItem = _stack.Pop();
+
+        Assert.AreEqual(10, poppedItem);
+    }
+
+    // Count Method
+    [TestMethod]
+    public void Count_WithEmptyStack_ReturnsZero()
+    {
+        var count = _stack.Count();
+        Assert.AreEqual(0, count);
+    }
+
+    [TestMethod]
+    public void Count_AfterPush_IncreasesCount()
+    {
+        _stack.Push(10);
+        var count = _stack.Count();
+        Assert.AreEqual(1, count);
+    }
+
+    [TestMethod]
+    public void Count_AfterPop_DecreasesCount()
+    {
+        _stack.Push(10);
+        _stack.Pop();
+
+        var count = _stack.Count();
+
+        Assert.AreEqual(0, count);
+    }
+
+    [TestMethod]
+    public void Count_AfterMultiplePushes_ReturnsCorrectCount()
+    {
+        _stack.Push(10);
+        _stack.Push(20);
+        _stack.Push(30);
+
+        var count = _stack.Count();
+
+        Assert.AreEqual(3, count);
+    }
+    
+    [TestMethod]
+    public void Pop_OnEmptyStack_ThrowsStackCantBeEmptyException()
+    {
+       Assert.ThrowsException<CustomStack.StackCantBeEmptyException>(() => _stack.Pop());
     }
 }
