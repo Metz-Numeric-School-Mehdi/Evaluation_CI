e4bf03369596b97e688a3d1ffd8e23fdeb57a136
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-01-22 16:01:53 +0100

    test

--- EvaluationTestUnits/TestUnits.cs
+++ EvaluationTestUnits/TestUnits.cs
@@ -14,6 +14,8 @@ public sealed class TestUnits
     }
     
     //Tests Math Operation
+    
+    // Add Method
     [TestMethod]
     [DataRow(14, 22, 36)]
     [DataRow(20, 23, 43)]
@@ -23,4 +25,26 @@ public sealed class TestUnits
         var result = _mathOperations.Add(numberOne, numberTwo);
         Assert.AreEqual(expectedResult, result);
     }
+    
+    // Divide Method
+    [TestMethod]
+    [DataRow(10, 10, 1)]
+    [DataRow(8, 2, 4)]
+    [DataRow(30, 3, 10)]
+    public void Divide_WithTwoNumbers_ReturnsDivide(int numberOne, int numberTwo, int expectedResult)
+    {
+        var result = _mathOperations.Divide(numberOne, numberTwo);
+        Assert.AreEqual(expectedResult, result);
+    }
+    
+    //Get Odd Number Method
+    // [TestMethod]
+    // [DataRow(10, 10, 1)]
+    // [DataRow(8, 2, 4)]
+    // [DataRow(30, 3, 10)]
+    // public void GetOddNumbers_WithOddNumber_ReturnsOddNumberList(int numberOne, int numberTwo, int expectedResult)
+    // {
+    //     var result = _mathOperations.Divide(numberOne, numberTwo);
+    //     Assert.AreEqual(expectedResult, result);
+    // }
 }
